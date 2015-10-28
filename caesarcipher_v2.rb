9c31b96e16aca6a8aee8ba5684309af7a99dea88
Jerome Sardoma <Jerome@jeromes-mbp.fios-router.home>
2015-10-28 16:03:41 -0700

    Updated solution to caesarcipher.rb

---
+++ caesarcipher_v2.rb
@@ -0,0 +1,40 @@
+#first create a hash that maps each letter of the alphabet to its corresponding letter after the shift
+
+def cipher_hash(shift)
+    alphabet = ("a".."z").to_a
+    letter_index = alphabet.index(letter)
+    alphabet_shift = Array.new
+    alphabet.each { |letter|
+        alphabet_shift << alphabet[letter_index - shift]
+    }
+    cipher = Hash(alphabet.zip(alphabet_shift)) #creates hash from array of array pairs
+    return cipher
+end
+
+def caesar_cipher(string, shift)
+    ciphered_string = ""
+    hash = cipher_hash(shift)
+    for i in 0..string.length do
+        current_letter = string[i].to_s
+        if current_letter.match(/[[:blank:]]/).nil? == false #NOT nil, i.e. there's a match for blank
+            ciphered_string[i] = " "
+        elsif current_letter.match(/[[:punct:]]/).nil? == false
+            ciphered_string[i] = current_letter
+        elsif current_letter.match(/[[:upper:]]/).nil? == false
+            lookup = hash[current_letter.downcase].to_s
+            ciphered_string[i] = lookup.upcase
+        else
+            lookup = hash[current_letter].to_s
+            ciphered_string[i] = lookup
+        end
+    end
+    return ciphered_string
+end
+
+puts "Enter a phrase."
+user_input = gets.chomp
+puts "Now enter the number of characters to shift."
+shift_number = gets.chomp
+
+puts caesar_cipher(user_input, shift_number.to_i).to_s
+
